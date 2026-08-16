576e220ac16e86b65e249de5f31255a5c6a3e83f
josthan12 <josthan2004wong@gmail.com>
2026-08-16 19:09:46 +0800

    Changed name and added Banner

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# ShrekAndDonkey User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/ShrekAndDonkey.java
@@ -0,0 +1,20 @@
+public class ShrekAndDonkey {
+    public static void main(String[] args) {
+        String divider = "____________________________________________________________";
+        String banner = " ____  _              _        _              _ ____              _\n"
+                + "/ ___|| |__  _ __ ___| | __   / \\   _ __   __| |  _ \\  ___  _ __ | | _____ _   _\n"
+                + "\\___ \\| '_ \\| '__/ _ \\ |/ /  / _ \\ | '_ \\ / _` | | | |/ _ \\| '_ \\| |/ / _ \\ | | |\n"
+                + " ___) | | | | | |  __/   <  / ___ \\| | | | (_| | |_| | (_) | | | |   <  __/ |_| |\n"
+                + "|____/|_| |_|_|  \\___|_|\\_\\/_/   \\_\\_| |_|\\__,_|____/ \\___/|_| |_|_|\\_\\___|\\__, |\n"
+                + "                                                                            |___/\n";
+
+        System.out.println(divider);
+        System.out.print(banner);
+        System.out.println("Grrr GET OUT OF MY SWAMP! Ohh, I didn't see you " +
+                "there stranger! I'm ShrekAndDonkey.");
+        System.out.println("What can I do for you?");
+        System.out.println(divider);
+        System.out.println("FEE FIE FOE FUMP. GET OUT OF MY SWAMP!");
+        System.out.println(divider);
+    }
+}
