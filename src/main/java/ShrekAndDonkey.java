274e9aa6e05dd12e23c629f99658141c61eb14d5
josthan12 <josthan2004wong@gmail.com>
2026-08-20 16:49:50 +0800

    Level 1. Echo

--- src/main/java/ShrekAndDonkey.java
+++ src/main/java/ShrekAndDonkey.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class ShrekAndDonkey {
     public static void main(String[] args) {
         String divider = "____________________________________________________________";
+        //Created using ChatGPT
         String banner = " ____  _              _        _              _ ____              _\n"
                 + "/ ___|| |__  _ __ ___| | __   / \\   _ __   __| |  _ \\  ___  _ __ | | _____ _   _\n"
                 + "\\___ \\| '_ \\| '__/ _ \\ |/ /  / _ \\ | '_ \\ / _` | | | |/ _ \\| '_ \\| |/ / _ \\ | | |\n"
@@ -13,6 +16,23 @@ public class ShrekAndDonkey {
         System.out.println("Grrr GET OUT OF MY SWAMP! Ohh, I didn't see you " +
                 "there stranger! I'm ShrekAndDonkey.");
         System.out.println("What can I do for you?");
+        System.out.println(divider);
+
+        //Scanning for input by user
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        //Scans till bye is input
+        while (!input.equals("bye")) { 
+            System.out.println(divider);
+            
+            //left padding String
+            String str = String.format("%10s",input);
+            System.out.println(str);
+            System.out.println(divider);
+            input = scanner.nextLine();
+        }
+
         System.out.println(divider);
         System.out.println("FEE FIE FOE FUMP. GET OUT OF MY SWAMP!");
         System.out.println(divider);
